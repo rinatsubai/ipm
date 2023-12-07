db07484fd461d3a4bfca00f6dc065f5b03aebdda
rinatsubaev <project@rep.earth>
2023-12-08 00:38:54 +0300

    commit before apisearch

--- ipmfinance/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def finance_page(request):
-    return render(request, 'finance_page.html', {})
--- management/commands/clientshandler.py
+++
@@ -1,9 +0,0 @@
-from ipmclients.models import *
-from ipmalpha.models import *
-
-def clients_projects(Client, Projects):
-    projects_list = Project.objects.all()
-    clients_list = Client.objects.all()
-    for client in clients_list:
-        Project.objects.filter(project_client = client)
-        print(Project, Client)
